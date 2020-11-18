4c28f7e2759786f1ab32a5858e0061a8407c27be
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-11-18 12:26:45 +0100

    Return Behemoth from CreatureFactory

--- src/main/java/pl/sdk/gui/CreatureFactory.java
+++ src/main/java/pl/sdk/gui/CreatureFactory.java
@@ -1,4 +1,13 @@
 package pl.sdk.gui;
 
 public class CreatureFactory {
+    static Creature create(String aCreatureName) {
+        switch(aCreatureName) {
+            case "Behemoth":
+                return new Creature(160, 50, 17, "Behemoth", 6, 6,
+                        new ReduceArmorPercentageCalculateStrategy(60));
+            default:
+                throw new UnsupportedOperationException("Not recognized creature" + aCreatureName);
+        }
+    }
 }
--- src/test/java/pl/sdk/gui/CreatureFactoryTest.java
+++ src/test/java/pl/sdk/gui/CreatureFactoryTest.java
@@ -1,4 +1,15 @@
 package pl.sdk.gui;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CreatureFactoryTest {
+
+    @Test
+    void shouldCreateBehemoth(){
+        Creature beh = CreatureFactory.create("Behemoth");
+
+        assertEquals("Behemoth", beh.getName());
+    }
 }
