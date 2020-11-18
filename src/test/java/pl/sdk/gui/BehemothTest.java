34267905bac9fc32df06f508631da51eb414fd51
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-11-18 10:21:04 +0100

    Test for Behemoth

---
+++ src/test/java/pl/sdk/gui/BehemothTest.java
@@ -0,0 +1,19 @@
+package pl.sdk.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BehemothTest {
+
+    @Test
+    void behemothShouldIgnore80PercentOfDefence() {
+        //given
+        Creature behemoth = new Creature(100,200, 100, "Behemoth",1, 1, new DamageCalculator());
+        Creature defender = new Creature(1000,100, 100, "Defender",11, 1);
+        //when
+        behemoth.attack(defender);
+        //then
+        assertEquals(820, defender.getCurrentHp());
+    }
+}
