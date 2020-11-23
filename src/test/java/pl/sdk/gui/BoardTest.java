d5beb51d7a3b43d77f8d375e7943e28f27c027a8
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-11-23 13:07:11 +0100

    Add creature to not empty field should throw IAE test

--- src/test/java/pl/sdk/gui/BoardTest.java
+++ src/test/java/pl/sdk/gui/BoardTest.java
@@ -35,10 +35,8 @@ class BoardTest {
     void shouldXWhenYouAddCreatureToNotEmptyField() {
         board.add(new Point(0, 0), creature);
         Creature creature2 = new Creature(10, 1, 1, "Creature 2", 1, 10);
-        board.add(new Point(0, 0), creature2);
 
-        Creature creatureFromBoard = board.get(0, 0);
-
-        assertEquals(creature, creatureFromBoard);
+        assertThrows(IllegalArgumentException.class,
+                () -> board.add(new Point(0, 0), creature2));
     }
 }
